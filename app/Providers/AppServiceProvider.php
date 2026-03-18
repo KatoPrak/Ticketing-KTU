bd5b02100763b029c122a1a0f22a685f4dca1e6d
KatoPrak <irvanronaldi2@gmail.com>
2026-03-18 10:37:52 +0700

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,11 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Force HTTPS di production
-        if (config('app.env') === 'production') {
-            URL::forceScheme('https');
-        }
-
         // 🔧 Inject variabel global ke semua view IT & Staff
         View::composer(['it.*', 'staff.*'], function ($view) {
             $view->with([
